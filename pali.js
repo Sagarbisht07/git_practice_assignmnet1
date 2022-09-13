77bc793a561b2e73512191e501eacf46acd14f9a
Bhagwat Kale <bhagwatr5253@gmail.com>
2022-09-13 17:25:11 +0530

    Correct code

--- pali.js
+++ pali.js
@@ -1,7 +1,7 @@
 let str= "naman";
 let bag="";
-for(let i=str.lenght; i>0; i++){
-   bag=+str[i];
+for(let i=str.lenght-1; i>=0; i--){
+   bag+=str[i];
 }
 if(str==bag) console.log("yes");
 else console.log("no");
